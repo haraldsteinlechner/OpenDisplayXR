55a5a412784d6e47e93fe169e1d43d2871637598
Harald Steinlechner <haraldsteinlechner@gmail.com>
2024-11-09 19:55:46 +0100

    added xr streamout build;

--- 2_start_hello_xr_demo.bat
+++ 2_start_hello_xr_demo.bat
@@ -11,10 +11,15 @@ set P_OVERRIDE_ACTIVE_CONFIG=qwerty
 set QWERTY_ENABLE=1
 set XRT_DEBUG_GUI=1
 
+set XR_API_LAYER_PATH=bin\api_layers
+REM set XR_ENABLE_API_LAYERS=XR_APILAYER_LUNARG_api_dump
+REM set XR_ENABLE_API_LAYERS=XR_APILAYER_HAAGCH_gl_context_fix;XR_aardworx_streamout
+set XR_ENABLE_API_LAYERS=XR_aardworx_streamout
+
 REM Resize the Monado Window to fit across a dual monitor extended desktop layout
-start "Extended Desktop Resize" %~dp0\bin\extended_desktop_resize.exe
+REM start "Extended Desktop Resize" %~dp0\bin\extended_desktop_resize.exe
 
 REM Start the OpenXR Based hello_xr demo app
-%~dp0\bin\hello_xr\hello_xr.exe -G Vulkan
+%~dp0\bin\hello_xr\hello_xr.exe -G OpenGL
 
 pause
